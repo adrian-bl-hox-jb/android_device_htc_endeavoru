d081c99f8c606a218bf1d849c03ecb4398a72945
Adrian Ulrich <adrian@blinkenlights.ch>
2012-12-31 10:06:32 +0100

    super secret cpu-downclock method added

--- power/power.c
+++ power/power.c
@@ -18,6 +18,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 #define LOG_TAG "Endeavoru PowerHAL"
 #include <utils/Log.h>
@@ -25,7 +26,22 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-static void sysfs_write(char *path, char *s)
+#define MAX_POWER_PROFILES 5
+
+static int power_profiles[MAX_POWER_PROFILES][12] = {
+ /*   scaling_max_freq    boost_factor ccap_level,   ccap_st,  max_boost,    go_maxspeed                 */
+    { 1500000, 475000,    0, 2,        1300, 1200,    1, 1,    0, 250000,    85, 95 }, /* NVidia default */
+    {  880000, 204000,    0, 2,        1300, 1200,    1, 1,    0, 250000,    85, 95 }, /* Sane           */
+    {  640000, 204000,    0, 2,        1300, 1200,    1, 1,    0, 250000,    85, 95 }, /* ok             */
+    {  475000, 204000,    0, 2,        1300, 1200,    1, 1,    0, 250000,    85, 95 }, /* insane         */
+    {  340000, 204000,    0, 2,        1300, 1200,    1, 1,    0, 250000,    85, 95 }, /* stupid         */
+};
+
+
+static int *pp = power_profiles[0];
+
+
+static void sysfs_write(char *path, int value)
 {
     char buf[80];
     int len;
@@ -37,23 +53,40 @@ static void sysfs_write(char *path, char *s)
         return;
     }
 
-    len = write(fd, s, strlen(s));
+    snprintf(buf, sizeof(buf)-1, "%d", value);
+    len = write(fd, buf, strlen(buf));
     if (len < 0) {
         strerror_r(errno, buf, sizeof(buf));
         ALOGE("Error writing to %s: %s\n", path, buf);
     }
-
+    else {
+        ALOGI("Wrote %s to %s\n", buf, path);
+    }
     close(fd);
 }
 
 static void endeavoru_power_init(struct power_module *module)
 {
+    char buf[4] = { 0 };
+    int pprofile;
+
+    int fd = open("/data/misc/adrian_pp", O_RDONLY);
+    if( fd >= 0 ) {
+        read(fd, buf, sizeof(buf)-1);
+        close(fd);
+        pprofile = atoi(buf);
+        if(pprofile >= 0 && pprofile < MAX_POWER_PROFILES) {
+            ALOGI("power profile set to %d", pprofile);
+            pp = power_profiles[pprofile];
+        }
+    }
+
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time",
-                "30000");
+                30000);
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_maxspeed_load",
-                "80");
+                80);
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/boost_factor",
-		"0");
+                0);
 }
 
 static void endeavoru_power_set_interactive(struct power_module *module, int on)
@@ -64,32 +97,30 @@ static void endeavoru_power_set_interactive(struct power_module *module, int on)
      */
 
     sysfs_write("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-                on ? "1500000" : "475000");
+                on ? pp[0] : pp[1]);
+    /* same as scaling_max_freq */
+    sysfs_write("/sys/module/cpu_tegra/parameters/cpu_user_cap",
+                on ? pp[0] : pp[1]);
 
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/boost_factor",
-                on ? "0" : "2");
+                on ? pp[2] : pp[3]);
 
-    sysfs_write("/sys/module/cpu_tegra/parameters/cpu_user_cap",
-                on ? "0" : "1200000");	
-    
     sysfs_write("/sys/kernel/tegra_cap/core_cap_level",
-                on ? "1300" : "1200");	
+                on ? pp[4] : pp[5]);	
 
-    sysfs_write("/sys/kernel/tegra_cap/core_cap_state", "1");	
+    sysfs_write("/sys/kernel/tegra_cap/core_cap_state",
+                on ? pp[6] : pp[7]); /* always enabled (?) */
     
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/max_boost",
-                on ? "0" : "250000");
+                on ? pp[8] : pp[9]);
 
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_maxspeed_load",
-                on ? "85" : "95");
+                on ? pp[10] : pp[11]);
 }
 
 static void endeavoru_power_hint(struct power_module *module, power_hint_t hint,
                             void *data)
 {
-    char buf[80];
-    int len;
-
     switch (hint) {
     case POWER_HINT_VSYNC:
         break;

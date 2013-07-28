cf1f3260f4a1746e792cd0bbba8a8e8d16bd7451
Adrian Ulrich <adrian@blinkenlights.ch>
2013-07-28 12:27:30 +0200

    initial 4.3 support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a9
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
